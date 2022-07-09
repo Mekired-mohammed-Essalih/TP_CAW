92e0ec583e238c998874f74b745c2897f2e4a0e4
Mekired-mohammed-Essalih <mohammedessalih.mekired@univ-constantine2.dz>
2022-07-09 01:33:12 +0100

    editeur web exemple

---
+++ script.js
@@ -0,0 +1,7 @@
+function playcodes(){
+    var codes= document.getElementById("coodes").value;
+    document.getElementById("output").innerHTML = codes;
+}
+function removecodes (){
+    document.getElementById("output").innerHTML="";
+}
